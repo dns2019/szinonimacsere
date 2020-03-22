06a80297661ca253d468a039750700ce5ad00ccc
dns2019 <49754632+dns2019@users.noreply.github.com>
2020-03-22 13:54:42 +0100

    GUI support for Auto-Synonym
    Implemented Tkinter drag and drop input system with a python library called TkinterDnD2.
    
    Input file: cucc2.txt
    Output file: kimenet.txt
    
    Based on: https://github.com/tothzalan/szinonimacsere

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#!/usr/bin/python3.6
+# Keszitette: Lajos Denes, Toth Zalan szinonima-szkriptje alapjan.
+import sys
+import io 
+import os
+if sys.version_info[0] == 2:
+	from Tkinter import *
+else:
+	from tkinter import *
+	from TkinterDnD2 import *
+
 def readin(filename: str):
     with open(filename) as f:
         content = f.read().splitlines()
@@ -12,20 +23,47 @@ def contains(arr, word):
     return False
 
 def replace(line, word):
-    spltln = line.split("|")
-    print(spltln)
-    for i in spltln:
-        if(i != word and i.isdigit() == False and len(i) > 1):
-            return i
-    return word
+	spltln = line.split("|")
+	print(spltln)
+	for i in spltln:
+		if(i != word and i.isdigit() == False and len(i) > 1):
+			return i
+	return word
 
-def main():
-    word = "előtérbe állít"
-    lines = readin("data.dat")
-    cnt = contains(lines,word)
-    if (cnt[0]):
-        print(replace(lines[cnt[1]],word))
             
 
-if __name__ == "__main__":
-    main()
+def drop(event):
+	entry_sv.set(event.data)
+	path = entry_sv.get()
+	print(entry_sv.get())
+	with open(path, 'r', encoding='utf8') as file:
+		text = file.read()#.replace('\n', '')
+	text_list = text.split(' ')
+	lines = readin("data.dat")
+	for x in range(0,len(text_list),2):
+		if text_list[x]!=' ' and text_list[x] != '.' and text_list[x] != '?' and text_list[x] != '!' and text_list[x] != ',':
+			print(text_list[x])
+			word = text_list[x]
+			cnt = contains(lines,word)
+			print(cnt)
+			if cnt:
+				print(replace(lines[cnt[1]],word))
+				text_list[x] = replace(lines[cnt[1]],word)
+	new_text_list = " ".join(text_list)
+	print(new_text_list)
+	#Ha nem kell fajlba kiirna, akkor ezt lehet torolni.
+	filename = os.path.dirname(path)
+	filename += "/kimenet.txt"
+	with io.open(filename,'w',encoding='utf8') as f:
+    		f.write(new_text_list)
+	print(filename)
+
+root = TkinterDnD.Tk()
+root.winfo_toplevel().title("Auto-Szinoníma")
+entry_sv = StringVar()
+entry_sv.set('Dobd csak ide azt a fájlt: ...')
+entry = Entry(root, textvar=entry_sv, width=80)
+entry.pack(fill=X, padx=10, pady=10)
+entry.drop_target_register(DND_FILES)
+entry.dnd_bind('<<Drop>>', drop)
+root.mainloop()
